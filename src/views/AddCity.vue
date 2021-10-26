785d6403a9a22670cf5f9a80c22cd98b4a6b9c31
Peter Sviryd <Peter.blr@yandex.ru>
2021-10-27 00:19:42 +0300

    add video and condition

--- src/views/AddCity.vue
+++ src/views/AddCity.vue
@@ -1,17 +1,36 @@
 <template>
-  <div class="home">
-
+  <div class="grid">
+    <div class="city-link" v-for="(city, index) in cities" v-bind:key="index">
+      <City v-bind:city="city" />
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 
+import City from "../components/City";
 
 export default {
-  name: 'AddCity',
+  name: "AddCity",
+  props: ["cities"],
+  created() {
+    console.log(this.cities);
+  },
   components: {
-
+    City
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  .grid {
+    display: grid;
+    padding-top: 81px;
+    background-color: #31363d;
+    width: 100%;
+    min-height: 100vh;
+    @media (min-width: 400px) {
+      grid-template-columns: repeat(2, 1fr );
+    }
+  }
+</style>
